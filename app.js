281ad17551363c5d8f15fb4c010d75d6c701ec4b
Amrom Steinmetz <amroms@gmail.com>
2016-11-08 11:58:09 -0500

    added some more code

--- app.js
+++ app.js
@@ -3,32 +3,16 @@ var chalk = require('chalk');
 var volleyball = require('volleyball');
 var nunjucks = require('nunjucks');
 var app = express();
+const routes = require('./routes');
 
-/*app.use('/', function(req, res, next) {
-  var method = chalk.blue(req.method);
-  var path = chalk.yellow(req.path);
-
-  console.log(method, path);
-  next();
-});*/
 
 app.set('view engine', 'html');
 app.engine('html', nunjucks.render);
 nunjucks.configure('views', {noCache: true});
 
-app.use(volleyball);
+app.use('/', routes);
 
-const people = [{name: 'Full'}, {name: 'Stacker'}, {name: 'Son'}];
-
-app.get('/', function(req, res) {
-  res.render('index', {title: 'Hall of Fame', people: people});
-});
-
-app.get('/news', function(req, res) {
-  res.send('Welcome to the news page');
-});
-
-const special = express.Router();
+/*const special = express.Router();
 
 app.use('/special', special);
 
@@ -39,21 +23,8 @@ special.use('/', function(req, res, next) {
 
 special.get('/cake', function(req, res, next) {
   res.send('This is a special birthday cake');
-});
+});*/
 
 app.listen(3000, function() {
   console.log('Listening on port 3000');
 });
-
-/*var locals = {
-    title: 'An Example',
-    people: [
-        { name: 'Gandalf'},
-        { name: 'Frodo' },
-        { name: 'Hermione'}
-    ]
-};*/
-
-/*nunjucks.render('index.html', locals, function (err, output) {
-    console.log(output);
-});*/
